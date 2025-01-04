415f7a6acb12cb204ccf487d33f29af0344a6062
davejohn150 <davejohn150@gmail.com>
2025-01-04 20:42:38 +0800

    getting seed from sqlite save

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 -- script.lua
 function luaFunction()
-    print("Hello from Lua!")
+    print("The seed is:")
+    print(seed)
+    -- NOTE this does not actually load the seed from c++, either call the get seed functions from lua instead or grab it from global
 end
--- sm_mapgen/sm_mapgen/sm_mapgen.cpp
+++ sm_mapgen/sm_mapgen/sm_mapgen.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
 #include <string>
+#include <sqlite3.h>
+#include <vector>
 
 #include <lua.hpp> 
 
 using namespace std;
 
-/* 
-TODO
-- make global classes for lua to utilise
-
-sm.terrainData.load() -- if I can get this i wont actually need to mix it with lua
 
 
-- json dump the g_cellData object 
-- json dump in the format expected by the mapping tool
+int getSQLiteSeed(const char* dbFile) {
+    sqlite3* db;
+    sqlite3_stmt* stmt;
 
+    // Open the SQLite database
+    if (sqlite3_open(dbFile, &db) != SQLITE_OK) {
+        std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
+        return 0;
+    }
 
-*/
+    const char* sql = "SELECT seed FROM Game;";  
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) != SQLITE_OK) {
+        std::cerr << "Failed to prepare statement: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return 0;
+    }
 
+    int seed = 0;
 
-void loadSQL(string filepath) {
-    cout << filepath;
+    // Execute the query and retrieve results
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+        seed = sqlite3_column_int(stmt, 0);
+
+        cout << seed << endl;
+    }
+
+    // Finalize the statement and close the database
+    sqlite3_finalize(stmt)
+        ;
+    sqlite3_close(db);
+    return seed;
 }
 
 
@@ -32,10 +51,15 @@ int main(int argc, char* argv[]) {
         filepath = argv[1];
     }
     else {
+        cout << "Enter path of survival save  (Save/Survival/EXAMPLE.db):" << endl;
         cin >> filepath;
     }
 
-    loadSQL(filepath);
+    const int seed = getSQLiteSeed(filepath.c_str());
+    if (seed == 0) {
+        cerr << "Failed to get game seed";
+        return 1;
+    }
 
 
     // Initialize the Lua interpreter
@@ -51,7 +75,7 @@ int main(int argc, char* argv[]) {
     // Load and execute a Lua script
     const char* scriptFile = "../../main.lua"; 
     if (luaL_dofile(L, scriptFile) != LUA_OK) {
-        std::cerr << "Error running script: " << lua_tostring(L, -1) << std::endl;
+        cerr << "Error running script: " << lua_tostring(L, -1) << endl;
         lua_close(L);
         return -1;
     }
@@ -61,7 +85,7 @@ int main(int argc, char* argv[]) {
     if (lua_isfunction(L, -1)) {
         // Call the Lua function
         if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
-            std::cerr << "Error calling Lua function: " << lua_tostring(L, -1) << std::endl;
+            cerr << "Error calling Lua function: " << lua_tostring(L, -1) << endl;
         }
     }
 
